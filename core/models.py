4cf8c61dd0e3863cced0e0a231aa138057687e0a
amirriazizadeh <ariazizade@gmail.com>
2025-09-08 15:06:58 +0330

    Add core app with BaseModel

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """
+    Abstract base model with common fields and logical delete
+    """
+    created_at = models.DateTimeField(default=timezone.now, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False, db_index=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+    def delete(self, using=None, keep_parents=False):
+        """
+        Logical delete: mark the record as deleted instead of removing it
+        """
+        self.is_deleted = True
+        self.save(using=using)
+
+    def hard_delete(self, using=None, keep_parents=False):
+        """
+        Permanently delete the record from the database
+        """
+        super().delete(using=using, keep_parents=keep_parents)
