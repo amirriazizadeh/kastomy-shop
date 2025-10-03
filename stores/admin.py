dfe8125a0f3a8f6f8e6d9f19dafaa81a1354a6b0
amirriazizadeh <ariazizade@gmail.com>
2025-10-03 15:17:16 +0330

    fix changes

--- stores/admin.py
+++ stores/admin.py
@@ -49,12 +49,7 @@ class StoreAdmin(admin.ModelAdmin):
     actions = [soft_delete, restore_items, activate_items, deactivate_items]
 
     from django.contrib import admin
-from .models import Store
 
-@admin.register(Store)
-class StoreAdmin(admin.ModelAdmin):
-    list_display = ("name", "owner", "is_active")
-    search_fields = ("name", "owner__username")
 
     # فقط کسانی که پرمیشن view_store دارند می‌بینند
     def has_view_permission(self, request, obj=None):
