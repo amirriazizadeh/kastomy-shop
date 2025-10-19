075b730700523b44a947f3dbaf6d85a932ee910d
amirriazizadeh <ariazizade@gmail.com>
2025-10-19 16:50:24 +0330

    delete some extra files

--- cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.MyCartView.as_view(), name='mycart'),  # /api/mycart/
-    path('items/', views.MyCartItemsView.as_view(), name='mycart-items'),  # /api/mycart/items/
-    path('add_to_cart/<int:id>/', views.AddToCartView.as_view(), name='add-to-cart'),  # /api/mycart/add_to_cart/<id>/
-    path('items/<int:id>/', views.CartItemDetailView.as_view(), name='cart-item-detail'),  # /api/mycart/items/<id>/
-]
--- stores/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Store, StoreItem
-
-admin.site.register(Store)
-admin.site.register(StoreItem)
