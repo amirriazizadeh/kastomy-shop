c9be0b2b0f84c7120240f7c1d3c7458121c16b1c
amirriazizadeh <ariazizade@gmail.com>
2025-10-20 17:59:15 +0330

    payments app

--- apps/payments/urls.py
+++ apps/payments/urls.py
@@ -1,5 +1,7 @@
+from apps.payments.views import PaymentList, ZarinPalResultPayment
 from django.urls import path
 
-
-
-urlpatterns =[]
+urlpatterns = [
+    path("", PaymentList.as_view(), name="list_payments"),
+    path("verify/", ZarinPalResultPayment.as_view(), name="zarinpal_result"),
+]
